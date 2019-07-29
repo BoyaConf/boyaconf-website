bf6932a48e13dcb1264fb37a9261d9365f4be239
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-07-29 14:17:32 -0500

    Improve speakers section. Update subscribe form

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,3 +53,66 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+function showFormError() {
+  try {
+    document.getElementById('sub-error').classList.remove('is-hidden');
+  } catch (e) {
+  }
+}
+
+function clearFormError() {
+  const errorEle = document.getElementById('sub-error');
+  errorEle.classList.add('is-hidden');
+}
+
+function validateForm(event) {
+  event.preventDefault();
+
+  const subNameField = document.getElementById('fields[name]');
+  const subName = subNameField ? subNameField.value || '' : '';
+  if (subName.length <= 0) {
+    showFormError();
+    return false;
+  }
+
+  const subEmailField = document.getElementById('fields[email]');
+  const subEmail = subEmailField ? subEmailField.value || '' : '';
+  if (subEmail.length <= 0) {
+    showFormError();
+    return false;
+  }
+
+  const response = grecaptcha.getResponse();
+  if (response.length === 0) {
+    showFormError();
+    return false;
+  }
+  clearFormError();
+
+  const subBtn = document.getElementById('sub-btn');
+  subBtn.classList.add('is-loading');
+
+  subNameField.disabled = true;
+  subEmailField.disabled = true;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://api.mailerlite.com/api/v2/groups/49639168/subscribers', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('X-MailerLite-ApiKey', 'e19e306ef466923adcdbb080bae8c734');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      subNameField.disabled = false;
+      subEmailField.disabled = false;
+      subBtn.classList.remove('is-loading');
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
+        console.error('Error:');
+        console.error(xhr.responseText);
+      }
+    }
+  };
+  xhr.send(JSON.stringify({ name: subName, email: subEmail }));
+  return true;
+}
